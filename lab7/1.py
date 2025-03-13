9ac860b528b1ba7c38857288525a1872022e71bf
Samat <samat2667@gmail.com>
2025-03-13 15:22:24 +0500

    update 1.py

--- lab7/1.py
+++ lab7/1.py
@@ -1,3 +1,50 @@
+# import pygame
+# import datetime
+
+# pygame.init()
+
+# screen = pygame.display.set_mode((1280,720))
+
+# mick = pygame.image.load('image/clock.png')
+# min = pygame.image.load('image/min_hand.png')
+# sec = pygame.image.load('image/sec_hand.png')
+
+# mick = pygame.transform.scale(mick, (533, 400))
+# min = pygame.transform.scale(min, (533, 400))
+# sec = pygame.transform.scale(sec, (533, 400))
+
+# min_rect = min.get_rect(center=(606,420))
+# sec_rect = sec.get_rect(center=(606,420))
+
+# game = True
+
+# while game:
+    
+#     date = datetime.datetime.now()
+#     second = date.second
+#     minute = date.minute
+    
+#     sec_angle = -second * 6
+#     min_angle = -minute * 6
+        
+#     min_rot = pygame.transform.rotate(min, min_angle) 
+#     min_rect = min_rot.get_rect(center=(606,420))
+    
+#     sec_rot = pygame.transform.rotate(sec, sec_angle)
+#     sec_rect = sec_rot.get_rect(center=(606,420))
+    
+#     screen.fill("white")     
+#     screen.blit(mick, (340, 210))
+#     screen.blit(min_rot, min_rect.topleft)
+#     screen.blit(sec_rot,  sec_rect.topleft)
+    
+#     pygame.display.update()
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             game = False
+
+# pygame.quit()
+
 import pygame
 
 pygame.init()
@@ -17,13 +64,13 @@ min_rect = min.get_rect(center=(606,420))
 sec_rect = sec.get_rect(center=(606,420))
 
 game = True
-angle = 55
-angle2 = -5
+angle = 60
+angle2 = -6
 
 while game:
     
-    if angle % 360 == 55: 
-        angle2 -=5
+    if angle % 360 == 60: 
+        angle2 -= 6
         
     min_rot = pygame.transform.rotate(min, angle2) 
     min_rect = min_rot.get_rect(center=(606,420))
@@ -36,7 +83,7 @@ while game:
     screen.blit(min_rot, min_rect.topleft)
     screen.blit(sec_rot,  sec_rect.topleft)
     
-    angle -= 5
+    angle -= 6
     clock.tick(1)
     
     pygame.display.update()
